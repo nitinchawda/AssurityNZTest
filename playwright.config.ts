dcca55f9f4eb6a0cb9bf2dfe0707c805a99ece8c
Nitin Chawda <nitinc@backbase.com>
2026-06-07 22:59:22 +0530

    added package.json and udpated playwright config json

---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: [['html', { open: 'never' }], ['list']],
+  use: {
+    trace: 'on-first-retry',
+  },
+});
